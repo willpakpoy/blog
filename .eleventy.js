9cd2bedfcc01dd1b87031903b378cea055a78cc7
Will Pak Poy (Surface 2020) <0521858@radford.act.edu.au>
2021-04-24 21:17:20 +1000

    fix passthrough folders

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ const pluginSass = require("eleventy-plugin-sass");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("static");
+    eleventyConfig.addPassthroughCopy({ "uploads": "static/uploads" });
+    eleventyConfig.addPassthroughCopy({ "static/robots.txt": "robots.txt" });
     eleventyConfig.addWatchTarget("./styles/**/*");
     eleventyConfig.addPlugin(pluginSass, {sourcemaps: true});
 
